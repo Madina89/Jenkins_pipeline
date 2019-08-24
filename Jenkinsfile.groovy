8fe888544133cb1dbc7c276a3092fd628a78204d
Madina89 <madinalinux89@gmail.com>
2019-08-24 11:28:42 -0500

    appche added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,10 @@
 node {
-    stage("Say Hello"){
+    stage("Pull Repo"){
         properties([parameters([string(defaultValue: '34.254.184.223', description: '''Dev:34.254.184.223 Qa:52.213.196.101 Prod:63.33.45.101''', name: 'Remote_Instance', trim: false)])])
-         git 'https://github.com/Madina89/jenkinsapril.git'
+         git 'git https://github.com/Madina89/Jenkins_Infra.git'
+    }
+    stage("Install Apache"){
+        sh "ssh ec2-user@${Remote_instances} sudo yum install httpd -y"
     }
 }
     
