07b64120ed87815bfa4622c5c638dd09123293a3
Madina89 <madinalinux89@gmail.com>
2019-08-24 11:13:15 -0500

    git added

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,7 +1,6 @@
 node {
     stage("Say Hello"){
-        properties([parameters([string(defaultValue: 'Madina', description: 'Paste your name', name: 'NAME', trim: true)]), pipelineTriggers([cron('* * * * * ')])])
-        sh "echo Hello ${NAME}"
+    git 'https://github.com/Madina89/Jenkins_Infra.git'
     }
 
        
