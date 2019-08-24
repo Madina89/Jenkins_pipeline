3f956a48c314d60d5e378975242b1d2eee5fe818
Madina89 <madinalinux89@gmail.com>
2019-08-24 11:24:45 -0500

    added name

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,7 @@
 node {
     stage("Say Hello"){
-         git 'https://github.com/Nurjan87/jenkinsapril.git'
+        properties([parameters([string(defaultValue: '34.254.184.223', description: '''Dev:34.254.184.223 Qa:52.213.196.101 Prod:63.33.45.101''', name: 'Remote_Instance', trim: false)])])
+         git 'https://github.com/Madina89/jenkinsapril.git'
     }
 }
     
